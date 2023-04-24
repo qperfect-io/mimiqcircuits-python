0371a9e7b55ad747ddf57ae27bc6fe1e5ec4c9b1
Guido Masella <guido.masella@gmail.com>
2023-04-24 18:05:57 +0200

    python: added mimiqlink dependency

--- mimiqcircuits/__init__.py
+++ mimiqcircuits/__init__.py
@@ -4,5 +4,8 @@ import mimiqcircuits.gates as gates
 from mimiqcircuits.gates import *
 from mimiqcircuits.circuit import *
 
+from mimiqlink import MimiqConnection
+
+
 # re export from the two previous
-__all__ = circuit.__all__ + gates.__all__
+__all__ = circuit.__all__ + gates.__all__ + ['MimiqConnection']
